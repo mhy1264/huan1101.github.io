5ed408e9e95b0a37c4c8a59704e4ba2efc0beeed
Ming Huan Yang <mhy1264@gmail.com>
2022-10-05 17:15:09 +0800

    update URL

--- URL.js
+++ URL.js
@@ -1,1 +1,1 @@
-const URL="";
+const URL="https://13f2-218-32-46-141.jp.ngrok.io";
--- test.sh
+++ test.sh
@@ -3,6 +3,10 @@ IP=$(curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url)
 
 echo "$IP"
 
-cat>url.txt<< EOF
+cat>URL.js<< EOF
 const URL="$IP";
 EOF
+
+git add .
+git commit -m "update URL"
+git push
