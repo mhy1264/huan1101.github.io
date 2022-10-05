8501a6654720c48e3be748c6fdf5a98a57408207
Ming Huan Yang <mhy1264@gmail.com>
2022-10-05 21:41:43 +0800

    update URL

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ IP=$(curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url)
 echo "$IP"
 
 cat>URL.js<< EOF
-const URL="$IP//auth-sign-in?appUri=%2F";
+const URL="$IP/auth-sign-in?appUri=%2F";
 EOF
 
 git add .
